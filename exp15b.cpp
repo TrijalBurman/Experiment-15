ce05c4dc4c59bf090b9fbd62d13e416fd507191d
TrijalBurman <62022917+TrijalBurman@users.noreply.github.com>
2024-10-20 23:35:48 +0530

    Add files via upload

---
+++ exp15b.cpp
@@ -0,0 +1,24 @@
+//Trijal Burman
+//23070123145
+
+#include <iostream>
+using namespace std;
+int add(int n)
+{
+    if (n<=1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (n+add(n-1));
+    }
+}
+int main()
+{
+    int x,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    x= add(n);
+    cout<<x;
+}
---
+++ exp15c.cpp
@@ -0,0 +1,23 @@
+//Trijal Burman
+//23070123145
+
+#include <iostream>
+#include <string>
+using namespace std;
+void rev(char *str)
+{
+    if (*str)
+    {
+        rev(str+1);
+        cout<<("%c",*str);
+
+    }
+
+}
+int main()
+{
+    char s[50];
+    cout<<"Enter a string: ";
+    cin>>s;
+    rev(s);
+}
